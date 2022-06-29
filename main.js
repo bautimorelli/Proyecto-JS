d3e52f787b437557f49cb92527390ce18a4ce0d3
Bautista Morelli <bautistamorelli@gmail.com>
2022-06-29 18:52:31 -0300

    Entrega Eventos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Classes....
 class Operation {
-	constructor(num1, num2, operator, result) {
+	constructor(num1 = " ", num2 = " ", operator = " ", result) {
 		this.num1 = num1;
 		this.num2 = num2;
 		this.operator = operator;
@@ -20,117 +20,84 @@ class Operation {
 	}
 }
 
+
 //Main....
 const results = [];
-newOperation();
+let operation = new Operation()
+const mainDisplay = document.getElementById("mainDisplay")
+const subDisplay = document.getElementById("subDisplay")
+const history = document.getElementById("history")
+
+
+//Events....
+for (let index = 0; index < 10; index++) {
+	document.getElementById("num" + index).onclick = () => {addNumerToDisplay(String(index))}
+}
+
+document.getElementById("changeSign").onclick = () => {
+	let content = mainDisplay.innerHTML
+	if (content[0] != "-") {
+		mainDisplay.innerHTML = "-" + content
+	} else {
+		mainDisplay.innerHTML = content.substring(1)
+	}
+}
+
+document.getElementById("comma").onclick = () => {
+	let content = mainDisplay.innerHTML
+	if (!content.includes(".")) {
+		mainDisplay.innerHTML = content + "."
+	}
+}
+
+document.getElementById("clear").onclick = () => {mainDisplay.innerHTML = ""}
+
+document.getElementById("plus").onclick = () => {operatorClick("+")}
+document.getElementById("substract").onclick = () => {operatorClick("-")}
+document.getElementById("multiply").onclick = () => {operatorClick("*")}
+document.getElementById("divide").onclick = () => {operatorClick("/")}
+document.getElementById("power").onclick = () => {operatorClick("^")}
+
+document.getElementById("clearAll").onclick = () => {clearAll()}
+
+document.getElementById("equal").onclick = () => {equal()}
+
 
 //Functions....
-function newOperation() {
-	let number1 = prompt("Ingresa un numero:");
-	number1 = Number(number1);
-	number1 = validateNumber(number1);
 
-	let number2 = prompt("Ingresa un segundo numero:");
-	number2 = Number(number2);
-	number2 = validateNumber(number2);
-
-	let operator = prompt(
-		"Ingresa que operacion quiere realizar: \n1) Suma \n2) Resta \n3) Multiplicacion \n4) Division \n5) Potencia (se tomara el segundo numero como indice)"
-	);
-	operator = Number(operator);
-
-	const answer = calculate(number1, number2, operator);
-	alert("El resultado es: " + answer);
-	operator = operatorCharacter(operator);
-	results.push(new Operation(number1, number2, operator, answer));
-
-	if (window.confirm("Desea realizar otra operacion?")) {
-		newOperation();
-	} else {
-		let parent = document.getElementById("history");
-		results.forEach((operation) => {
-			operation.toHTML(parent);
-		});
+function addNumerToDisplay (number) {
+	if(subDisplay.innerHTML.includes("=")){
+		subDisplay.innerHTML = ""
+		mainDisplay.innerHTML = ""
 	}
+	let content = mainDisplay.innerHTML
+	content = content + number
+	mainDisplay.innerHTML = content
 }
 
-function validateNumber(number) {
-	while (isNaN(number)) {
-		console.log(isNaN(number));
-		alert("Numero no valido");
-		number = prompt("Ingresa un numero valido:");
-		number = Number(number);
-	}
-	return number;
+function operatorClick (operator) {
+	subDisplay.innerHTML = mainDisplay.innerHTML + " " + operator + " "
+	operation.num1 = Number(mainDisplay.innerHTML)
+	operation.operator = operator
+	mainDisplay.innerHTML = ""
 }
 
-function calculate(number1, number2, operator) {
-	switch (operator) {
-		case 1:
-			return addition(number1, number2);
-
-		case 2:
-			return subtraction(number1, number2);
-
-		case 3:
-			return multiplication(number1, number2);
-
-		case 4:
-			return division(number1, number2);
-
-		case 5:
-			return power(number1, number2);
-
-		default:
-			alert("Operacion no valida");
-			operator = prompt(
-				"Ingresa que operacion quiere realizar: \n1 = suma \n2 = resta \n3 = multiplicacion \n4 = division \n5 = potencia (se tomara el segundo numero como indice)"
-			);
-			operator = Number(operator);
-			return calculate(number1, number2, operator);
-	}
+function clearAll () {
+	subDisplay.innerHTML = ""
+	operation.num1 = " "
+	operation.operator = " "
+	mainDisplay.innerHTML = ""
 }
 
-function operatorCharacter(operator) {
-	switch (operator) {
-		case 1:
-			return "+";
-		case 2:
-			return "-";
-		case 3:
-			return "*";
-		case 4:
-			return "/";
-		case 5:
-			return "^";
-	}
-}
+function equal() {
+	let equation = subDisplay.innerHTML + mainDisplay.innerHTML
+	operation.num2 = Number(mainDisplay.innerHTML)
+	subDisplay.innerHTML = equation + "="
+	const result = eval(equation)
+	operation.result = result
+	mainDisplay.innerHTML = result
 
-//Operations....
-function addition(number1, number2) {
-	return number1 + number2;
-}
-
-function subtraction(number1, number2) {
-	return number1 - number2;
-}
-
-function multiplication(number1, number2) {
-	return number1 * number2;
-}
-
-function division(number1, number2) {
-	if (number2 != 0) {
-		return number1 / number2;
-	} else {
-		return "Infinito";
-	}
-}
-
-function power(number, exponent) {
-	let result = 1;
-	for (i = 0; i < exponent; i++) {
-		result = result * number;
-	}
-	return result;
+	results.push(operation)
+	operation.toHTML(history)
+	operation = new Operation()
 }
